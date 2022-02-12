93dea1968a735b12776dcf6f18dfd4f503a31e20
Josh Zappone <zappone.josh@gmail.com>
2022-02-11 16:07:13 -0800

    remove map and read yaml from file

--- main/main.go
+++ main/main.go
@@ -12,29 +12,21 @@ func main() {
 	mux := defaultMux()
 
 	// Build the MapHandler using the mux as the fallback
-	pathsToUrls := map[string]string{
-		"/urlshort-godoc": "https://godoc.org/github.com/gophercises/urlshort",
-		"/yaml-godoc":     "https://godoc.org/gopkg.in/yaml.v2",
-	}
-	mapHandler := urlshort.MapHandler(pathsToUrls, mux)
+	// pathsToUrls := map[string]string{
+	// 	"/urlshort-godoc": "https://godoc.org/github.com/gophercises/urlshort",
+	// 	"/yaml-godoc":     "https://godoc.org/gopkg.in/yaml.v2",
+	// }
+	// mapHandler := urlshort.MapHandler(pathsToUrls, mux)
 
-	// Build the YAMLHandler using the mapHandler as the
-	// fallback
-	yaml := `
-- path: /urlshort
-  url: https://github.com/gophercises/urlshort
-- path: /urlshort-final
-  url: https://github.com/gophercises/urlshort/tree/solution
-`
-
-	// open yaml file and get []byte
 	yamlUrls, err := os.ReadFile("urls.yaml")
 	if err != nil {
 		os.Exit(2)
 		fmt.Printf("Oh no! Error: %s", err)
 	}
 
-	yamlHandler, err := urlshort.YAMLHandler([]byte(yaml), mapHandler)
+	// json urls
+
+	yamlHandler, err := urlshort.YAMLHandler(yamlUrls, mux)
 	if err != nil {
 		panic(err)
 	}
